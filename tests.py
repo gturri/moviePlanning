08fcb460836746ab960e4c198febe28e22caef77
Guillaume Turri <guillaume.turri@gmail.com>
2012-05-27 19:35:01 +0200

    Testing the effect of the parameter "time between showings"

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import unittest
 from moviePlanning import *
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class TestPlanning(unittest.TestCase):
   def test_std(self):
@@ -37,6 +37,27 @@ class TestPlanning(unittest.TestCase):
     self.assertTrue(seeMovie0ButNot1 or seeMovie1ButNot0)
     self.assertFalse(seeMovie0ButNot1 and seeMovie1ButNot0)
 
+  def test_timeBetweenShowings(self):
+    showings = []
+    showings.append(Showing(1, 1, datetime(2012, 1, 20,  8, 00), datetime(2012, 1, 20,  9, 00)))
+    showings.append(Showing(2, 1, datetime(2012, 1, 20, 10, 00), datetime(2012, 1, 20, 11, 00)))
+    showings.append(Showing(3, 1, datetime(2012, 1, 20, 12, 00), datetime(2012, 1, 20, 13, 00)))
+
+    solverForAFastSpectator = Solver(showings, timedelta(0, 30*60))
+    solverForASlowSpectator = Solver(showings, timedelta(0, 90*60))
+
+    showingsOfTheFastOne = solverForAFastSpectator.whichShowingsShouldIAttend()
+    showingsOfTheSlowOne = solverForASlowSpectator.whichShowingsShouldIAttend()
+
+    #Needing only 30 minutes between the end of a showings and the beginning of the next one, it is possible to see every movies
+    self.assertTrue(showings[0] in showingsOfTheFastOne)
+    self.assertTrue(showings[1] in showingsOfTheFastOne)
+    self.assertTrue(showings[2] in showingsOfTheFastOne)
+
+    #Needing 90 minutes between the end of a showings and the beginning of the next one, it is possible to see only movies 0 and 2
+    self.assertTrue(showings[0] in showingsOfTheSlowOne)
+    self.assertFalse(showings[1] in showingsOfTheSlowOne)
+    self.assertTrue(showings[2] in showingsOfTheSlowOne)
 
 
 if __name__ == '__main__':
